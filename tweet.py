6467a2d3cf9286a406de3dda518df642abfe9ebb
DhaivtV <vipatdhaivat25@gmail.com>
2023-07-06 20:09:46 +0530

    first commit

--- tweet.py
+++ tweet.py
@@ -39,6 +39,6 @@ if __name__ == "__main__":
             with open("logs.txt", "a+", encoding='utf-8') as f:
                 f.seek(0)  # Move the file pointer to the beginning
                 data = f.read()
-                f.write("\n"+str(e))
+                f.write("\n"+str(e)+f"{give_time()}")
             print("Error, trying again")
 
